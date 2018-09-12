c2329485a028d17f7dd4958c83e3c52df166c049
Trogg <ivanbialotski@gmail.com>
2018-09-12 09:34:04 +0300

    feat(AppConfig): create project with spring context configuration file

---
+++ src/main/java/messenger/Application.java
@@ -0,0 +1,7 @@
+package messenger;
+
+public class Application {
+  public static void main(String[] args) {
+    //
+  }
+}
---
+++ src/main/java/messenger/config/AppConfig.java
@@ -0,0 +1,8 @@
+package messenger.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("messenger")
+public class AppConfig {}
